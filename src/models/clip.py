07d08d7bceeb051c2ad1d89ef608fe79f0cfa622
morrisflorek <morris.benedikt.florek@uni-weimar.de>
2023-04-30 23:07:19 +0200

    CLIP model

---
+++ src/models/clip.py
@@ -0,0 +1,27 @@
+import clip
+import open_clip
+import torch
+from torch.nn import Module
+
+
+class CLIPModel(Module):
+    def __init__(self, model_name, device, jit=False):
+        super(CLIPModel, self).__init__()
+        self.device = device
+        self.model, self.preprocess = clip.load(model_name, device=device, jit=jit)
+
+    def forward(self, x: torch.Tensor):
+        x = self.preprocess(x).unsqueeze(0).to(self.device)
+        x = self.model.encode_image(x)
+        return x
+
+
+class CLIPModelPretrained(Module):
+    def __init__(self, model_name, pretrained_model):
+        super(CLIPModelPretrained, self).__init__()
+        self.model, _, self.preprocess = open_clip.create_model_and_transforms(model_name, pretrained_model)
+
+    def forward(self, x: torch.Tensor):
+        x = self.preprocess(x).unsqueeze(0)
+        x = self.model.encode_image(x)
+        return x
